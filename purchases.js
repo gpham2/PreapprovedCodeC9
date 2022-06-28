3299c92044085d11681d26ac8407d586c349570a
gpham2 <giangphamgia03@gmail.com>
2022-06-28 12:51:26 -0700

    updated for 6/28, contains createStringList and bascially done Purchase

--- purchases.js
+++ purchases.js
@@ -1,71 +1,81 @@
 
-
-
 const {
-    API_TOKEN_C9TG: token,
+  API_TOKEN_C9TG: token,
 } = process.env;
 
 
 const getPurchases = async (req, res) => {
 
-    const id = req.query.id;
+  const id = req.query.id;
 
-    const store = 'c9testingground';
+  const store = 'c9testingground';
 
-    const resultTrue2 = await fetch(
-        `https://${store}.myshopify.com/admin/api/2022-01/graphql.json`,
-        {
-          body: `
-              {
-                customer(id: "gid://shopify/Customer/${id}") {
-                  id,
-                  email,
-                  firstName,
-                  lastName,
-                  orders(first: 10) {
-                    edges {
-                        node {
-                            id,
-                            name,
-                            processedAt,
-                            displayAddress {
-                                address1,
-                                city,
-                                country
-                            },
-                            subtotalPriceSet {
-                                presentmentMoney {
-                                    currencyCode,
-                                    amount
-                                }
+  const resultTrue2 = await fetch(
+      `https://${store}.myshopify.com/admin/api/2022-01/graphql.json`,
+      {
+        body: `
+            {
+              customer(id: "gid://shopify/Customer/${id}") {
+                id,
+                email,
+                firstName,
+                lastName,
+                orders(first: 10) {
+                  edges {
+                      node {
+                          id,
+                          name,
+                          processedAt,
+                          displayAddress {
+                              address1,
+                              city,
+                              country
+                          },
+                          subtotalPriceSet {
+                              presentmentMoney {
+                                  currencyCode,
+                                  amount
+                              }
+                          }
+                          lineItems(first: 10) {
+                            edges {
+                              node {
+                                id,
+                                name,
+                                quantity,
+                                vendor,
+                              }
                             }
-                        }
-                    }
+                          }
+                      }
                   }
                 }
               }
-              
-          `,
-          headers: {
-            'Content-Type': 'application/graphql',
-            'X-Shopify-Access-Token': token,
-          },
-          method: 'POST',
+            }
+            
+        `,
+        headers: {
+          'Content-Type': 'application/graphql',
+          'X-Shopify-Access-Token': token,
         },
-    );
+        method: 'POST',
+      },
+  );
+ 
+  const data = await resultTrue2.json();
+  
+  console.log("print: " + data.body);
 
-    const data = await resultTrue2.json();
-    
-    return res
-      .status(200)
-      .json({ message: `Your id: ${id}`, data, status: 'success' });
+  return res
+    .status(200)
+    .json({ message: `Your id: ${id}`, data, status: 'success' });
 }
 
 export default (req, res) => {
-    switch (req.method) {
-      case 'GET':
-        return getPurchases(req, res);
-      default:
-        return res.status(405).json({ message: 'Method not allowed.', status: 'error' });
-    }
-  };
+  switch (req.method) {
+    case 'GET':
+      return getPurchases(req, res);
+    default:
+      return res.status(405).json({ message: 'Method not allowed.', status: 'error' });
+  }
+};
